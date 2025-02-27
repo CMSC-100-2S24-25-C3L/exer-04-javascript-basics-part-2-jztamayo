14f436b59ff6b540c647d7b9598ac37641e1494c
Johannes Tamayo <jztamayo@up.edu.ph>
2025-02-27 15:06:52 +0800

    test: added more test cases and invalid tests

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 import { generateUniqueID, addAccount } from './index.js'
 addAccount(["Alan", "Turing", "aturing@w3c.com", 58]);
 addAccount(["Johannes", "Tamayo", "jztamayo@up.edu.ph", 58]);
-addAccount(["Miku", "Hatsune", "hmiku@vocaloid.net", 58]);
+addAccount(["Miku", , "hmiku@vocaloid.net", 58]);
+addAccount(["", "Hatsune", "hmiku@vocaloid.net", 58]);
+addAccount(["Miku", "Hatsune", "hmiku@vocaloid.net", 17]);
+addAccount(["Miku", "Hatsune", "hmikuvocaloid.net", 17]);
+addAccount(["Lu8kla", "Megurine", "gmekfd@vocaloid.net", 18]);

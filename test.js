3cb575d41eae09dbfc9faf2a3faaa82e0c7d4a7c
Johannes Tamayo <jztamayo@up.edu.ph>
2025-02-27 14:56:05 +0800

    test: added more test cases

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 import { generateUniqueID, addAccount } from './index.js'
+addAccount(["Alan", "Turing", "aturing@w3c.com", 58]);
+addAccount(["Johannes", "Tamayo", "jztamayo@up.edu.ph", 58]);
+addAccount(["Miku", "Hatsune", "hmiku@vocaloid.net", 58]);
